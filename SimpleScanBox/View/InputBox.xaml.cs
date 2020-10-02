8601b63cb1dda90198b75c212a7a41bc2af669f4
andreyamironov <40858348+andreyamironov@users.noreply.github.com>
2020-10-02 22:54:38 +0300

    Добавьте файлы проекта.

---
+++ SimpleScanBox/View/InputBox.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleScanBox.View
+{
+    /// <summary>
+    /// Логика взаимодействия для InputBox.xaml
+    /// </summary>
+    public partial class InputBox : Window
+    {
+        SimpleScan.Core.TypeScan TypeScan = SimpleScan.Core.TypeScan.None;
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+        public InputBox(SimpleScan.Core.TypeScan _typeScan) : this()
+        {
+            this.TypeScan = _typeScan;
+            SetHeader(_typeScan);
+        }
+
+        private void Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                DialogResult = true;
+                this.Close();
+            }
+        }
+        private void SetHeader(SimpleScan.Core.TypeScan _typeScan)
+        {
+            if (_typeScan == SimpleScan.Core.TypeScan.Pallet)
+            {
+                this.Title = "Паллет";
+            }
+            else if (_typeScan == SimpleScan.Core.TypeScan.Carton)
+            {
+                this.Title = "Короб";
+
+            }
+            this.Header.Text = this.Title;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FocusManager.SetFocusedElement(this, Input);
+        }
+    }
+}
---
+++ SimpleScanBox/ViewModel/Manual.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+using System.Windows.Shapes;
+using System.Windows.Xps.Packaging;
+
+namespace SimpleScanBox.ViewModel
+{
+    /// <summary>
+    /// Логика взаимодействия для Manual.xaml
+    /// </summary>
+    public partial class Manual : Window
+    {
+        public Manual()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("pack://application:,,,/SimpleScan_Manual.xps");
+            var stream = Application.GetResourceStream(uri).Stream;
+            Package package = Package.Open(stream);
+            PackageStore.AddPackage(uri, package);
+            var xpsDoc = new XpsDocument(package, CompressionOption.Maximum, uri.AbsoluteUri);
+            var fixedDocumentSequence = xpsDoc.GetFixedDocumentSequence();
+            doc.Document = fixedDocumentSequence; 
+            xpsDoc.Close();
+        }
+    }
+}
